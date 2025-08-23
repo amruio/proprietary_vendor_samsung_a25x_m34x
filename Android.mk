4555cf60b273f0776991d1d92706fd2df16810f1
Ksawlii <maja.blaszczak@proton.me>
2025-08-23 14:35:46 +0200

    m34x: Initial proprietary-firmware.txt list
    Change-Id: Ib14bd79fa226c5eeebe56a2982e67a114f2ea224
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- Android.mk
+++ Android.mk
@@ -6,5 +6,18 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),m34x)
 
+$(call add-radio-file-sha1-checked,radio/fld.bin_SM-M346B2,f0452dd6f54c04d677a1d5425fcb817173f87aa2)
+$(call add-radio-file-sha1-checked,radio/harx.bin_SM-M346B2,790ac591c5ca995efb0be2de7dae926f4d04987f)
+$(call add-radio-file-sha1-checked,radio/keystorage.bin_SM-M346B2,a57c3884aab160f3dbe72a495c1c20ba3f9a9e9b)
+$(call add-radio-file-sha1-checked,radio/ldfw.img_SM-M346B2,090e4b07961e7011a4a707cca113a22c6a6f4bda)
+$(call add-radio-file-sha1-checked,radio/models,f617dfd2b5d419de34f249fbbbd124eb846a12c7)
+$(call add-radio-file-sha1-checked,radio/modem.bin_SM-M346B2,616d774e3e7385913ca84ec5ef4cc7269e6be354)
+$(call add-radio-file-sha1-checked,radio/modem_debug.bin_SM-M346B2,c8ede46483dffd7cfa7e01ee8035274a65ab77ca)
+$(call add-radio-file-sha1-checked,radio/sboot.bin_SM-M346B2,8dc8c14edfd4f9d3ad186830d7f58ca15e3dd562)
+$(call add-radio-file-sha1-checked,radio/tzar.img_SM-M346B2,a50cd5f9f615f9771cb1453bb1c3ab3cfc2eff5b)
+$(call add-radio-file-sha1-checked,radio/tzsw.img_SM-M346B2,e772c0263eec00076739d6ec72b4d4cdb25d0ae0)
+$(call add-radio-file-sha1-checked,radio/uh.bin_SM-M346B2,de267c969be1baeead75f04b21a40161a8d7ed81)
+$(call add-radio-file-sha1-checked,radio/up_param.bin_SM-M346B2,d51154595d632425cb41d9ae0b497aa2228d8538)
+$(call add-radio-file-sha1-checked,radio/version_SM-M346B2,38c5004a920cc74c1495c2e63ecaad6034bb5a0d)
 
 endif
